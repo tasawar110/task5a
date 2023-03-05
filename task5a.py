f4fa0e964957624fce3ef16f87cfa19e6791a1b8
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-05 00:59:31 -0800

    Add files via upload

---
+++ task5a.py
@@ -0,0 +1,28 @@
+#task1
+a=input('enter your email address:')
+b=a.endswith('@mail.com')
+print(b)
+print(a)
+print('you have entered incorrect email')
+#task2
+a=input('your good name :')
+print('hello!:',a)
+print(input('I hope  you are doin well,how can we help you?:'))
+b=input('share your problem with us:')
+print(b)
+print('Thanks for your feedback, we will resolve your problem')
+#task3
+a=input('enter your URL:')
+b=a.startswith('http://www.')
+print(b)
+print(a)
+#task4
+a=input('enter your password:')
+b=a.isalnum()
+print(b)
+print(a)
+#task5
+a=input('enter you first name:')
+b=input('enter you father name:')
+c=input('enter you caste name:')
+print(a,b)
